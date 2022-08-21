8714c6515abb9eaed6bbc2219e23be0d860664bf
Jannes-dev <jannes.platz@web.de>
2022-08-21 17:26:00 +0200

    Test case added

---
+++ CarReservationService2/test/test/AutheticationTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AutheticationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
