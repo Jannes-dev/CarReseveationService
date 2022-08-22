ee36dd1df7341787c898d53b15c622c1827b7fc3
Jannes-dev <jannes.platz@web.de>
2022-08-22 18:38:59 +0200

    ff

---
+++ CarReservationService2/test/test/PaymentTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PaymentTest {
+	
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
